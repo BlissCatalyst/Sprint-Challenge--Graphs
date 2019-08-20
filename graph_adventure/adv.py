5df4bc557c68b8c984a0e286a5a86526f3517a23
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-08-19 21:14:32 -0600

    as far as I could

--- graph_adventure/adv.py
+++ graph_adventure/adv.py
@@ -2,6 +2,8 @@ from room import Room
 from player import Player
 from world import World
 
+from util import Stack
+
 import random
 
 # Load world
@@ -20,10 +22,28 @@ world.loadGraph(roomGraph)
 world.printRooms()
 player = Player("Name", world.startingRoom)
 
-# roomGraph = {0: [(3, 5), {'n': 1}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}]}
+# roomGraph={0: [(3, 5), {'n': 1, 's': 5, 'e': 3, 'w': 7}], 1: [(3, 6), {'s': 0, 'n': 2, 'e': 12, 'w': 15}], 2: [(3, 7), {'s': 1}], 3: [(4, 5), {'w': 0, 'e': 4}], 4: [(5, 5), {'w': 3}], 5: [(3, 4), {'n': 0, 's': 6}], 6: [(3, 3), {'n': 5, 'w': 11}], 7: [(2, 5), {'w': 8, 'e': 0}], 8: [(1, 5), {'e': 7}], 9: [(1, 4), {'n': 8, 's': 10}], 10: [(1, 3), {'n': 9, 'e': 11}], 11: [(2, 3), {'w': 10, 'e': 6}], 12: [(4, 6), {'w': 1, 'e': 13}], 13: [(5, 6), {'w': 12, 'n': 14}], 14: [(5, 7), {'s': 13}], 15: [(2, 6), {'e': 1, 'w': 16}], 16: [(1, 6), {'n': 17, 'e': 15}], 17: [(1, 7), {'s': 16}]}
 # FILL THIS IN
 traversalPath = ['n', 's']
+def walking_graph():
+    s = Stack()
 
+    visited = set()
+    visited.add(0)
+    print(roomGraph[0][1]['n'])
+    s.push(roomGraph[0][1]['n'])
+
+    while s.size() > 0:
+        room = s.pop()
+        print(f'room: {room}')
+        if room not in visited:
+            print(f'room: {room}')
+            visited.add(room)
+
+            for next_room in roomGraph[room]:
+                s.push(next_room)
+
+walking_graph()
 
 # TRAVERSAL TEST
 visited_rooms = set()
