a6efb61f808cd25779c191ca879505ed871bc6aa
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-09-13 11:41:02 -0600

    back to brute force. Contains function to log a list of directions to go where we came from

--- graph_adventure/adv.py
+++ graph_adventure/adv.py
@@ -37,12 +37,23 @@ def room_init():
         traversal_graph[player.currentRoom.id][exit_dir] = '?'
 
 def contains_question():
-        # Just finds out if and unknown exit exists
+        # Just finds out if an unknown exit exists
     for n_e_s_w in player.currentRoom.getExits():
+        
         if traversal_graph[player.currentRoom.id][n_e_s_w] == '?':
             return True
         else:
             return False
+
+def opposite_dir(direction):
+    if direction == 'n':
+        return 's'
+    elif direction == 's':
+        return 'n'
+    elif direction == 'w':
+        return 'e'
+    elif direction == 'e':
+        return 'w'
 
 room_init()
 
@@ -50,20 +61,14 @@ trail = list()
 
 while len(traversal_graph) < len(roomGraph):
     travel_dir = ''
-    contains_unknown = False
     unknown = list()
 
-    if contains_question():
-        for n_e_s_w in player.currentRoom.getExits():
-            unknown.append(n_e_s_w)
-        travel_dir = random.choice(unknown)
-    else:
-
-        travel_dir = random.choice(player.currentRoom.getExits())
+    travel_dir = random.choice(player.currentRoom.getExits())
     
     prev_room = player.currentRoom.id
     player.travel(travel_dir)
     traversalPath.append(travel_dir)
+    trail.append(opposite_dir(travel_dir))
     if player.currentRoom.id not in traversal_graph:
         room_init()
     traversal_graph[prev_room][travel_dir] = player.currentRoom.id
